f9c599646dc3900f212ded5e407930db5a6b486a
Garland <garlandpotts1@gmail.com>
2019-08-27 10:47:52 -0400

    Update sass.js
    Remove autoprefixer error

--- gulp-tasks/sass.js
+++ gulp-tasks/sass.js
@@ -5,15 +5,7 @@ module.exports = function(gulp, browserSync) {
         return function sassTask(cb){
             gulp.src(['app/scss/styles.scss']) // Gets all files ending with .scss in app/scss
             .pipe(sass().on('error', sass.logError))
-            .pipe(autoprefixer({
-                      browsers: [
-                        '>1%',
-                        'last 4 versions',
-                        'Firefox ESR',
-                        'not ie < 9', // React doesn't support IE8 anyway
-                      ],
-                      flexbox: 'no-2009',
-                    }))
+            .pipe(autoprefixer())
             .pipe(gulp.dest('.tmp/css'))
             .pipe(browserSync.reload({
                 stream: true
